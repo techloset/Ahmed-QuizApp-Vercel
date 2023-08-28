bc230d92b0bbdbb09596c546114e66cd456a84e8
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-28 11:53:30 +0500

    first commit

---
+++ models/questions.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema, } from "mongoose";
+
+const questionSchema = new Schema({
+    
+        question: String,
+        option1: String,
+        option2: String,
+        option3: String,
+        correctOption:  String,
+    
+},{
+    timestamps: true
+})
+
+const Questions = mongoose.models.Questions || mongoose.model('Questions', questionSchema)
+export default Questions
